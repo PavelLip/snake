d612abdabbc60ad0982e057a05cc4cd22db4d86e
Pavel <zavod93@mail.ru>
2021-03-27 21:31:09 +0300

    add list

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,15 +10,30 @@ namespace snake
     {
         static void Main(string[] args)
         {
-
+            
             point p1 = new point(1, 3, '*');
             p1.draw();
 
             point p2 = new point(4, 5, '#');
             p2.draw();
 
+            point p3 = new point(2, 7, '&');
+            p3.draw();
 
+            point p4 = new point(3, 4, '^');
+            p4.draw();
             
+
+            List<point> plist = new List<point>();
+            plist.Add (p1);
+            plist.Add (p2);
+            plist.Add (p3);
+            plist.Add (p4);
+
+
+
+
+
             Console.ReadLine();
         }
      
